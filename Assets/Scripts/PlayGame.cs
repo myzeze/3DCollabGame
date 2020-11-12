3bda8f88f0e8b87c79bbf2191d5767be022cde0d
Xena <54689838+myzeze@users.noreply.github.com>
2020-11-12 20:22:30 +0000

    push

--- Assets/Scripts/PlayGame.cs
+++ Assets/Scripts/PlayGame.cs
@@ -5,17 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class PlayGame : MonoBehaviour
 {
-    
+    //do not change !!!
     public void playgame ()
     {
         SceneManager.LoadScene("GameScene");
     }
 
+    //do not change !!!
     public void settingspage()
     {
         SceneManager.LoadScene("Settings");
     }
 
+    //do not change !!!
     public void exitgame()
     {
         Debug.Log("exitgame");

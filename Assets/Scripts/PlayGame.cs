e17ca949f615173405ca8e30b838658691a33a25
Xena <54689838+myzeze@users.noreply.github.com>
2020-11-12 20:19:26 +0000

    Main menu
    Main menu buttons and change scene base code and visuals

---
+++ Assets/Scripts/PlayGame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayGame : MonoBehaviour
+{
+    
+    public void playgame ()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void settingspage()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+
+    public void exitgame()
+    {
+        Debug.Log("exitgame");
+        Application.Quit();
+    }
+
+}

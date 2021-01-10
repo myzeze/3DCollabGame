80e407b0ede75bc86499091ec0b67e669cfc9eb9
AaronDhanoa <55580337+AaronDhanoa@users.noreply.github.com>
2021-01-10 15:28:32 +0000

    added Minimap
    added Minimap and script to project

---
+++ Assets/Scripts/MinimapScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapScript : MonoBehaviour
+{
+    public Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 newPosition = player.position;
+        newPosition.y = transform.position.y; //keeps y position the same
+        transform.position = newPosition;
+
+        transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f); //makes camera rotate with player
+    }
+}

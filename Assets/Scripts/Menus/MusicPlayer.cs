212ca4c19a7719acb98c9c7afea38f9dd15001c0
andr3y38 <56317287+andr3y38@users.noreply.github.com>
2021-01-11 00:25:30 +0000

    Settings for volume

---
+++ Assets/Scripts/Menus/MusicPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public AudioSource AudioSource;
+
+    private float musicVolume_f = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioSource.Play();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AudioSource.volume = musicVolume_f;
+    }
+    public void updateVolume(float volume)
+    {
+        musicVolume_f = volume;
+    }
+}
